3fff5ab0ea94613ff19eeffc89fc448d108da6f8
Oleg Gurev <dark-home@list.ru>
2021-06-27 10:41:39 +0300

    выделил вспом.класс для управления сессиями

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -10,11 +10,11 @@ def app(request):
     return fixture
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="new group", header="group header 1", footer="group footer 1"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
