05459bb3499bfd67dfd7531a2895d267d1fcae3f
Oleg Gurev <dark-home@list.ru>
2021-06-29 10:36:01 +0300

    в метод редактирования групп передается модель группы

--- fixture/group.py
+++ fixture/group.py
@@ -13,13 +13,10 @@ class GroupHelper:
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
-        wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
@@ -45,13 +42,14 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
 
-    def modify_first_group(self):
+    def modify_first_group(self, group):
         wd = self.app.wd
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("new group name")
-        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("new header")
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("update").click()
 
 
