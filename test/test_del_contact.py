d55f705f2faafa4b082cc01e4d18f5583d9e72b2
Oleg Gurev <dark-home@list.ru>
2021-06-29 10:03:22 +0300

    в метод удаления контакта добавлен переход на страницу контактов

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -2,5 +2,6 @@
 
 def test_delete_first_contact(app):
     app.session.login(username="admin", password="secret")
+    app.contact.go_to_home_page()
     app.contact.delete_first_contact()
     app.session.logout()
