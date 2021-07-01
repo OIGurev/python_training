31a8575540c6a49609a600b140e531b3b9b93ef0
Oleg Gurev <dark-home@list.ru>
2021-07-01 09:43:09 +0300

    выполнена оптимизация ввода данных в форму контакта, добавлены отдельные тесты на модификацию имени и отчества

--- fixture/contact.py
+++ fixture/contact.py
@@ -20,34 +20,27 @@ class ContactHelper:
         self.fill_contact_fields(contact)
         wd.find_element_by_name("update").click()
 
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
     def fill_contact_fields(self, contact):
         wd = self.app.wd
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
+        self.change_field_value("firstname", contact.firstname)
+        self.change_field_value("middlename", contact.middlename)
+        self.change_field_value("lastname", contact.lastname)
+        self.change_field_value("nickname", contact.nickname)
+        self.change_field_value("title", contact.title)
+        self.change_field_value("company", contact.company)
+        self.change_field_value("address", contact.address)
         #fill phone numbers
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home)
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.work)
+        self.change_field_value("home", contact.home)
+        self.change_field_value("mobile", contact.mobile)
+        self.change_field_value("work", contact.work)
         #fill e-mail
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
+        self.change_field_value("email", contact.email)
         #select birthday
         wd.find_element_by_name("bday").click()
         Select(wd.find_element_by_name("bday")).select_by_visible_text("17")
@@ -58,16 +51,9 @@ class ContactHelper:
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys("1999")
         #fill secondary information form
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact.address2)
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(contact.phone2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contact.notes)
-
-
+        self.change_field_value("address2", contact.address2)
+        self.change_field_value("phone2", contact.phone2)
+        self.change_field_value("notes", contact.notes)
 
     def delete_first_contact(self):
         wd = self.app.wd
--- test/test_modify_first_contact.py
+++ test/test_modify_first_contact.py
@@ -1,13 +1,16 @@
 from model.contact import Contact
 
 
-def test_modify_first_contact(app):
+def test_modify_first_contact_firstname(app):
     app.session.login(username="admin", password="secret")
     app.contact.go_to_home_page()
     app.contact.go_to_modify_page()
-    app.contact.modify_first_contact(Contact(firstname="Ivan", middlename="", lastname="",
-                       nickname="", title="", company="",
-                       address="", home="", mobile="",
-                       work="", email="", address2="",
-                       phone2="", notes=""))
+    app.contact.modify_first_contact(Contact(firstname="Ivan"))
+    app.session.logout()
+
+def test_modify_first_contact_middlename(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.go_to_home_page()
+    app.contact.go_to_modify_page()
+    app.contact.modify_first_contact(Contact(middlename="Petrovich"))
     app.session.logout()
