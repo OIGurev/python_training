f66c4cd7572cbaf059f164ac9d22f1527c9a409b
Oleg Gurev <dark-home@list.ru>
2021-07-01 08:25:12 +0300

    выделен метод заполнения полей группы общий для создания и редактирования

--- fixture/group.py
+++ fixture/group.py
@@ -12,16 +12,24 @@ class GroupHelper:
         self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
-        # fill group form
+        self.fill_group_fields(group)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def modify_first_group(self, group):
+        wd = self.app.wd
+        self.fill_group_fields(group)
+        wd.find_element_by_name("update").click()
+
+    def fill_group_fields(self, group):
+        wd = self.app.wd
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
 
     def delete_first_group(self):
         wd = self.app.wd
@@ -42,14 +50,6 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
 
-    def modify_first_group(self, group):
-        wd = self.app.wd
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("update").click()
+
 
 
