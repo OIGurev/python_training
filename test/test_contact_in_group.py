69d3a7cbf4c42354aae65c106f08c2ac54f3e32a
Oleg Gurev <dark-home@list.ru>
2021-08-18 09:44:46 +0300

    Задание 22 доработка 3
    переработал тест добавления контакта в группу

--- test/test_contact_in_group.py
+++ test/test_contact_in_group.py
@@ -24,17 +24,20 @@ def test_del_contact_from_group(app, orm):
     list_contacts_not_in_group = orm.get_contacts_not_in_group(del_group_from_contact)
     assert new_contacts == list_contacts_not_in_group
 
-
-def test_add_contact_to_group(app, db):
-    check_empty_filling(app, db)
-    contact_to_add = db.get_contacts_not_in_groups()[0]
-    group = db.get_group_list()[0].id
-    app.contact.add_contact_to_group_by_id(contact_to_add, group)
-    new_contacts_not_in_groups = db.get_contacts_not_in_groups()
-    assert contact_to_add not in new_contacts_not_in_groups
-
-def check_empty_filling(app, db):
-    if len(db.get_contact_list()) == 0:
-        app.contact.create(Contact(firstname="test"))
-    if len(db.get_group_list()) == 0:
-        app.group.create(Group(name="test"))
+def test_add_contact_to_group(app, orm):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="aa", middlename="aaa", lastname="aaaa", nickname="testnicka",
+                                   title="testtitlea", company="tca",
+                                   homephone="123432", mobile="89121111111", work="2-32-32",
+                                   email="test@test3.ru", email2="test2@test3.ru", email3="test3@test3.ru",
+                                   address="taa", phone2="132",
+                                   notes="tna"))
+    if app.group.count() == 0:
+        app.group.create(Group(name='Test'))
+    contacts = orm.get_contact_list()
+    groups = orm.get_group_list()
+    edit_contact = random.choice(contacts)
+    add_group_for_contact = random.choice(groups)
+    app.contact.add_contact_to_group_by_id(edit_contact.id, add_group_for_contact.id)
+    list_contacts_in_group = orm.get_contacts_in_group(add_group_for_contact)
+    assert edit_contact in list_contacts_in_group
